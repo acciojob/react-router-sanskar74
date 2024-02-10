5029038f1f3561ddeeb5cc65a07c59f97b7f1319
sanskarsahu <sanskarsahu1580@gmail.com>
2024-02-11 00:40:05 +0530

    new changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
 import React from "react";
-import './../styles/App.css';
-
+import "./../styles/App.css";
+import Home from "./Home";
+import About from "./About";
+import { Routes, Route, Link } from "react-router-dom";
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/About">About</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
